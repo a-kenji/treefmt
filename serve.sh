c2fc67d847470c2a2afed55bfc72faa393543116
a-kenji <a-kenji@users.noreply.github.com>
2023-09-22 11:28:40 +0000

    deploy: 1cf4ef35505aa04482078325e505d509d60c7ad3

--- serve.sh
+++
@@ -1,4 +0,0 @@
-#!/nix/store/p7bpdnxqd3i5hwm92mrscf7mvxk66404-bash-5.1-p16/bin/bash
-# Build and serve the docs for local development
-set -euo pipefail
-webfsd -d -r "$(nix-build "$(dirname "$0")/.." -A docs)"
